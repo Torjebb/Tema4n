7284d53022190566066789e3f958f908211a8895
Torjebb <torje94@gmail.com>
2021-12-15 16:44:16 +0100

    ferdig

--- src/components/Slides.vue
+++ src/components/Slides.vue
@@ -4,52 +4,52 @@
       <img :src="slide" alt="" >
     </div>
 
-  <button @click="next" class="btn btn__next">Next</button>
-  <button @click="prev" class="btn btn__prev">Prev</button>
+    <button @click="next" class="btn btn__next">Next</button>
+    <button @click="prev" class="btn btn__prev">Prev</button>
   
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            slides: [
-                'https://picsum.photos/id/238/1080/700',
-                'https://picsum.photos/id/236/1080/700',
-                'https://picsum.photos/id/235/1080/700',
-                'https://picsum.photos/id/234/1080/700',
-                'https://picsum.photos/id/233/1080/700',
-                
-            ],
-            visibleSlide: 0,
-        }
-    },
+    export default {
+        data() {
+            return {
+                slides: [
+                    'https://picsum.photos/id/238/1080/700',
+                    'https://picsum.photos/id/236/1080/700',
+                    'https://picsum.photos/id/235/1080/700',
+                    'https://picsum.photos/id/234/1080/700',
+                    'https://picsum.photos/id/233/1080/700',
+                    
+                ],
+                visibleSlide: 0,
+            }
+        },
 
-    computed: {
-        slidesLength() {
-            return this.slides.length
-        }
-    },
+        computed: {
+            slidesLength() {
+                return this.slides.length
+            }
+        },
 
-    methods: {
-        next() {
-          if(this.visibleSlide >= this.slidesLength - 1) {
-              this.visibleSlide = 0
-          } else {
-              this.visibleSlide++
-          }
-      },
-        prev() {
-          if(this.visibleSlide <= 0) {
-              this.visibleSlide = this.slidesLength - 1
-          } else {
-              this.visibleSlide--
-          }
-      }
+        methods: {
+            next() {
+            if(this.visibleSlide >= this.slidesLength - 1) {
+                this.visibleSlide = 0
+            } else {
+                this.visibleSlide++
+            }
+        },
+            prev() {
+            if(this.visibleSlide <= 0) {
+                this.visibleSlide = this.slidesLength - 1
+            } else {
+                this.visibleSlide--
+            }
+        }
+        }
+
     }
-
-}
-// <img src="@/assetts/name/of_your_image" /> tror dette skal funke for bilder lokalt
+    // <img src="@/assetts/name/of_your_image" /> tror dette skal funke for bilder lokalt
 
 
 
